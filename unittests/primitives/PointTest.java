6bdb7f6270667065dcd6711874bfaa177769bf87
ayalaimer <ayalacohen456@gmail.com>
2024-05-19 15:22:09 +0300

    Beginig of stage 2

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,30 @@
+package primitives;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @org.junit.jupiter.api.Test
+    void testSubtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDistanceSquared() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testTestToString() {
+    }
+}
