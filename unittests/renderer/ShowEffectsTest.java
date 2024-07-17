b0a5b72f78852c79e963bf535a7a4e0b773653c1
Orgilad <orestercohen@gmail.com>
2024-07-17 12:50:04 +0300

    7 Finished

--- unittests/renderer/ShowEffectsTest.java
+++ unittests/renderer/ShowEffectsTest.java
@@ -11,6 +11,10 @@ import lighting.AmbientLight;
 import primitives.*;
 import scene.Scene;
 
+/**
+ * This class is used to demonstrate various effects such as transparency, reflection,
+ * and shading using a simple scene.
+ */
 public class ShowEffectsTest {
 
     private final Scene scene = new Scene("Show Effects Scene");
@@ -18,22 +22,26 @@ public class ShowEffectsTest {
             .setDirection(new Vector(0, 0, -1), new Vector(0, 1, 0))
             .setRayTracer(new SimpleRayTracer(scene));
 
+    /**
+     * This test method creates a scene with various geometries and lights,
+     * then renders the scene to an image.
+     */
     @Test
     public void ShowEffects() {
         // Adding geometries to the scene
         scene.geometries.add(
                 new Sphere(new Point(0, 0, -50), 25d)
                         .setEmission(new Color(MAGENTA))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30).setKt(0.3).setKr(0.3)),
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30).setKt(0.4).setKr(0.3)),
                 new Triangle(new Point(-10, -80, -80), new Point(50, -50, -60), new Point(0, 50, -60))
                         .setEmission(new Color(GREEN))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)),
+                        .setMaterial(new Material().setKd(0.9).setKs(0.5).setShininess(60)),
                 new Sphere(new Point(100, 100, -300), 75d)
                         .setEmission(new Color(CYAN))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(50).setKt(0.3).setKr(0.3)),
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(40).setKt(0.6).setKr(0.3)),
                 new Sphere(new Point(50, 50, -100), 30d)
                         .setEmission(new Color(ORANGE))
-                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(50).setKt(0.3).setKr(0.3))
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(50).setKt(0.2).setKr(0.3))
         );
 
         // Adding lights to the scene
